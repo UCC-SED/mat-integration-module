bf640268172558c7afb578c3213f62a84912b14e
gyusto <geeyusto17@gmail.com>
2020-08-26 11:46:33 +0300

    Add DIN constant varibale

--- api/src/main/java/org/openmrs/module/integration/api/util/Constant.java
+++ api/src/main/java/org/openmrs/module/integration/api/util/Constant.java
@@ -5,5 +5,6 @@ public class Constant {
     public static final String order_status_new="NEW";
     public static final String order_status_cancelled="DISCONTINUE";
     public static final String order_status_changed="REVISE";
+    public  static  final  String meth_mat_map="MAT.meth.DIN";
 
 }
